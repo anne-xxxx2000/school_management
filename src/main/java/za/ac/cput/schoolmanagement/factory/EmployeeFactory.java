228f49fc6eefa2c68506b3ee78135ff73cf21e11
MichelMuemboIlunga <61247320+MichelMuemboIlunga@users.noreply.github.com>
2022-06-18 23:03:13 +0200

    Revert "Complete phase 1 / Emeka Thato Nwamadi 219404070"

--- src/main/java/za/ac/cput/schoolmanagement/factory/EmployeeFactory.java
+++ src/main/java/za/ac/cput/schoolmanagement/factory/EmployeeFactory.java
@@ -1,21 +1,4 @@
 package za.ac.cput.schoolmanagement.factory;
 
-/*
- * Author: Emeka Thato Nwamadi
- * Student number 219404070
- * Date: June
- */
-
-import za.ac.cput.schoolmanagement.domain.Employee;
-import za.ac.cput.schoolmanagement.util.helper;
-
 public class EmployeeFactory {
-    public static Employee createEmployee(String staffId, String email, String name) {
-
-//        Validation
-        if (helper.isEmptyOrNull(staffId) || helper.isEmptyOrNull(email) || helper.isEmptyOrNull(name))
-            throw new IllegalArgumentException("staff Id, email and name are required ");
-
-        return new Employee.Builder().setStaffId(staffId).setEmail(email).setEmployeeName(name).build();
-    }
 }
--- src/test/java/za/ac/cput/schoolmanagement/factory/EmployeeFactoryTest.java
+++ src/test/java/za/ac/cput/schoolmanagement/factory/EmployeeFactoryTest.java
@@ -1,10 +1,5 @@
 package za.ac.cput.schoolmanagement.factory;
 
-/*
- * Author: Emeka Thato Nwamadi
- * Student number 219404070
- * Date: June
- */
 
 import static org.junit.jupiter.api.Assertions.*;
 
