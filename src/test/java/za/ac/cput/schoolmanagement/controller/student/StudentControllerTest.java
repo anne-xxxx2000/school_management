5dbdefea208b5f83ebb15f4427b9a41ec90ce339
HonestMpungu <Mpunguhonest@gmail.com>
2022-06-18 20:41:14 +0200

    Committing test classes

---
+++ src/test/java/za/ac/cput/schoolmanagement/controller/student/StudentControllerTest.java
@@ -0,0 +1,24 @@
+package za.ac.cput.schoolmanagement.controller.student;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentControllerTest {
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void testGetStudent() {
+    }
+
+    @Test
+    void createStudent() {
+    }
+
+    @Test
+    void deleteStudent() {
+    }
+}
